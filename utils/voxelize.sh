06436c8ccd92b98282dd9d379f4b0f621aa42e1d
Sanjeev Mk <sanjeevmk4890@gmail.com>
2018-12-12 17:05:01 +0530

    fixed voxelizer

--- utils/voxelize.sh
+++ utils/voxelize.sh
@@ -2,8 +2,7 @@ shapenetroot=$1
 cwd=$(pwd)
 binvoxcmd=$cwd'/binvox'
 echo $binvoxcmd
-#for syn in {'02691156','03001627','02958343'}; do
-for syn in {'00000001','00000002'}; do
+for syn in {'02691156','03001627','02958343'}; do
 	cd $shapenetroot'/'$syn
 	for modeldir in */ ; do
 		cd $modeldir
